0c44147454dc71e9d1490cc0fdfe6af6e2b854bc
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-10-13 03:14:13 +0900

    Manage, Document, User

---
+++ Solution/PEngine/Controllers/ManageApiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PEngine.Controllers
+{
+    [ApiController]
+    [Route("/manage/[action]")]
+    [AutoValidateAntiforgeryToken]
+    public class ManageApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Solution/PEngine/Models/BlogContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PEngine.Models.Comment;
+using PEngine.Models.File;
+using PEngine.Models.Post;
+using PEngine.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PEngine.Models
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<CommentModel> Comments { get; set; }
+        public DbSet<FileModel> Files { get; set; }
+        public DbSet<PostModel> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("");
+    }
+}
