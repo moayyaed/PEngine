6c4a8a2f40da65761d4cdcaaab384ef8711e44bf
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-10-13 19:12:23 +0900

    Search Module

---
+++ Solution/PEngine/Models/Search/SearchModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PEngine.Models.Search
+{
+    public class SearchModel
+    {
+
+    }
+}
