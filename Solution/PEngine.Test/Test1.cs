fd33bc19a9d510b8750677107449237c6bbd9ca2
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-03 11:22:14 +0900

    Reconstruct Test Project

---
+++ Solution/PEngine.Test/Test1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PEngine.Test
+{
+    class Test1
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestA()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
