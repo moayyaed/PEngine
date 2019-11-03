df13804987af93c12d058fffd8bdb0c4b87a9382
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-03 15:56:16 +0900

    Factory class for Multiple DBMS Support

---
+++ Solution/PEngine/Models/Data/BlogContextFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PEngine.Models.Data
+{
+    public enum DBMSType
+    {
+        Postgresql, SQLite, SqlServer
+    }
+
+    public class BlogContextFactory
+    {
+        public static string        ConnectionString { get; set; }
+        public static BlogContext   Context { get; set; }
+
+        public static void          Initialize(DBMSType type)
+        {
+            if (Context != null) return;
+
+            switch (type)
+            {
+                case DBMSType.Postgresql:
+                    Context = new PostgresqlBlogContext();
+                    break;
+
+                case DBMSType.SQLite:
+                    Context = new SqliteBlogContext();
+                    break;
+
+                case DBMSType.SqlServer:
+                    Context = new SqlServerBlogContext();
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
