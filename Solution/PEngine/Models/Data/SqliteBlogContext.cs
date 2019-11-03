52d6c6d08ab86402217fa6c6f11d40fd7cedff35
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-03 15:55:34 +0900

    Blog Context for PostgreSQL and SQLite

---
+++ Solution/PEngine/Models/Data/SqliteBlogContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PEngine.Models.Data
+{
+    public class SqliteBlogContext : BlogContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite(BlogContextFactory.ConnectionString);
+    }
+}
