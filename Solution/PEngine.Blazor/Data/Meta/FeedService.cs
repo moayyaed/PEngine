9510314dd2ec76c144e6138cdf58a07e3bc26396
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-07 15:52:00 +0900

    Feed Models

---
+++ Solution/PEngine.Blazor/Data/Meta/FeedService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PEngine.Blazor.Data.Meta
+{
+    public class FeedService
+    {
+        public static string RequestAtomData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string RequestRSSData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
