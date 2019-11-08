3b70fdadb5777258c499617f669b523f7bb03c38
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-08 19:37:12 +0900

    CommentListModel

---
+++ Solution/PEngine/Models/Comment/CommentListModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PEngine.Models.Comment
+{
+    public class CommentListModel
+    {
+        public long PostId { get; set; }
+    }
+}
