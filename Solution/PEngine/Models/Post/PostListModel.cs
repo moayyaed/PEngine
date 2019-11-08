9551c594b158877a9e478ddff60103fdbcd5626b
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-08 18:21:00 +0900

    PostListModel, SearchRequestModel

--- Solution/PEngine/Models/Post/PostListModel.cs
+++ Solution/PEngine/Models/Post/PostListModel.cs
@@ -7,6 +7,13 @@ namespace PEngine.Models.Post
 {
     public class PostListModel
     {
-
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public long WriterId { get; set; }
+        public string WriterName { get; set; }
+        public DateTime WrittenAt { get; set; }
+        public string ThumbnailURL { get; set; }
+        public string Abstract { get; set; }
+        public long ReadCount { get; set; }
     }
 }
---
+++ Solution/PEngine/Models/Search/SearchRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PEngine.Models.Search
+{
+    public class SearchRequestModel
+    {
+        public string Filter { get; set; }
+        public bool SearchEntireSite { get; set; }
+        public string SearchScope { get; set; }
+        public string Keyword { get; set; }
+    }
+}
