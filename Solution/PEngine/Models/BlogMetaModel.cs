92757f855fb76edec186697a1a5fa2951170c149
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-15 20:48:33 +0900

    Add Theme Color to MetaModel

--- Solution/PEngine/Models/BlogMetaModel.cs
+++ Solution/PEngine/Models/BlogMetaModel.cs
@@ -8,6 +8,7 @@ namespace PEngine.Models
     public class BlogMetaModel
     {
         public string Title { get; set; }
+        public string ThemeColor { get; set; }
         public bool   UseGateway { get; set; }
         public string Field { get; set; }
         public string Layout { get; set; }
