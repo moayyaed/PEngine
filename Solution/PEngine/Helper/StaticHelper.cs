33475bda76257b19e9a3ced5a81eb1f83e726fd4
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-15 19:51:58 +0900

    Helper Class for Static Objects

---
+++ Solution/PEngine/Helper/StaticHelper.cs
@@ -0,0 +1,27 @@
+﻿using PEngine.Models;
+using PEngine.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PEngine.Helper
+{
+    public static class StaticHelper
+    {
+        private static BlogMetaModel meta;
+        public static bool MetaNeedUpdated { get; set; } = true;
+        public static BlogMetaModel Meta 
+        {
+            get
+            {
+                if (MetaNeedUpdated)
+                {
+                    meta = BlogContextFactory.Context.Metadata.First();
+                }
+
+                return meta;
+            }
+        }
+    }
+}
