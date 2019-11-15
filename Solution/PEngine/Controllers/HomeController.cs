751d6207112040ac15d485a044e04671512c6c64
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-15 19:51:48 +0900

    Error Handler & Home Controller for Testing

---
+++ Solution/PEngine/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PEngine.Models.Data;
+
+namespace PEngine.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BlogContext context;
+        
+        public HomeController(BlogContext _context)
+        {
+            context = _context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
