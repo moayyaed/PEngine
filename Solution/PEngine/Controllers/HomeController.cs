22bc77cf62a55f025a8f75985cb1db5cf46b87b0
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-19 20:13:05 +0900

    Gateway

--- Solution/PEngine/Controllers/HomeController.cs
+++ Solution/PEngine/Controllers/HomeController.cs
@@ -2,23 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PEngine.Helper;
 using PEngine.Models.Data;
 
 namespace PEngine.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ISession session;
         private readonly BlogContext context;
         
-        public HomeController(BlogContext _context)
+        public HomeController(BlogContext _context, HttpContextAccessor _accessor)
         {
-            context = _context;
+            session = _accessor.HttpContext.Session;
         }
 
         public IActionResult Index()
         {
-            return View();
+            if (StaticHelper.Meta.UseGateway && 
+                session.GetInt32("GatewayViewed") != 1)
+            {
+                session.SetInt32("GatewayViewed", 1);
+                return View("Gateway");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
