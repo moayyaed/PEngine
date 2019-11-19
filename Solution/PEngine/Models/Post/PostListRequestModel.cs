f2a26d9975f85bbeecd44a07480be0058f2d6057
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-11-19 20:13:40 +0900

    Post List Endpoint & Param Model

---
+++ Solution/PEngine/Models/Post/PostListRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PEngine.Models.Post
+{
+    public class PostListRequestModel
+    {
+        public string Category { get; set; }
+        public int Page { get; set; }
+    }
+}
