3b247ad235a50db7770f20a7a6358379b1a49fa2
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-12-21 19:06:14 +0900

    Add Files Relation

--- Solution/PEngine/Common/Components/Database/Contexts/BlogDbContext.cs
+++ Solution/PEngine/Common/Components/Database/Contexts/BlogDbContext.cs
@@ -8,15 +8,12 @@ namespace PEngine.Common.Components.Database.Contexts
         public DbSet<PostModel> Posts { get; set; }
         public DbSet<CommentModel> Comments { get; set; }
         public DbSet<UserModel> Users { get; set; }
+        public DbSet<FileModel> Files { get; set; }
 
         protected BlogDbContext()	
         {	
             Database.EnsureCreated();	
         }	
 
-        protected override void OnModelCreating(ModelBuilder builder)	
-        {	
-
-        }
     }
 }
