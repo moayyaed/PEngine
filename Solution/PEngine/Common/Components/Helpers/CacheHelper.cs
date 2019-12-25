616f62db082cb6f727a765bfcc94b47aa3634cd8
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-12-25 22:17:21 +0900

    Post CacheHelper & PostWriteRequestModel Extension Method

---
+++ Solution/PEngine/Common/Components/Helpers/CacheHelper.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using HeyRed.MarkdownSharp;
+
+namespace PEngine.Common.Components.Helpers
+{
+    public class CacheHelper
+    {
+        public static string CachePost(string contentType, string content)
+        {
+            if (string.IsNullOrEmpty(content))
+            {
+                return null;
+            }
+         
+            string cacheData;
+
+            switch (contentType)
+            {
+                case "markdown":
+                    cacheData = CacheMarkdown(content);
+                    break;
+                  
+                default:
+                    return null;
+            }
+            
+            
+            var path = $"~/Repositories/PostCaches/{contentType}/{Path.GetRandomFileName()}";
+            File.WriteAllText(path, cacheData);
+
+            return path;
+        }
+
+        private static string CacheMarkdown(string markdown)
+        {
+            return new Markdown().Transform(markdown);
+        }
+    }
+}
---
+++ Solution/PEngine/Modules/Blog/Models/Posts/PostWriteRequestModelExtension.cs
@@ -0,0 +1,35 @@
+using PEngine.Common.Components.Helpers;
+using PEngine.Common.Models.Schema;
+
+namespace PEngine.Modules.Blog.Models.Posts
+{
+    public static class PostWriteRequestModelExtension
+    {
+        public static PostModel CreatePostModel(this PostWriteRequestModel model)
+        {
+            if (string.IsNullOrEmpty(model.Title) || string.IsNullOrEmpty(model.Content))
+            {
+                return null;
+            }
+            
+            return new PostModel
+            {
+                Category = model.Category,
+                Title = model.Title,
+                
+                ContentType = model.ContentType,
+                ContentCachePath = string.IsNullOrEmpty(model.Content) ?
+                    CacheHelper.CachePost(model.ContentType, model.Content) : null,
+                Content = model.Content,
+                
+                Tags = model.Tags,
+                Files = model.Files,
+                
+                Private = model.Private,
+                Protected = model.Protected,
+                ProtectPassword = model.Protected ?
+                    CryptoHelper.Sha256(model.ProtectPassword) : null
+            };
+        }
+    }
+}
