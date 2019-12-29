62e27edb097f0e072f36506c6e0d092a87eea1da
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-12-30 00:45:57 +0900

    UserModelExtensions.UpdateUser to update specific model object

--- Solution/PEngine/Common/Models/SchemaExtensions/UserModelExtensions.cs
+++ Solution/PEngine/Common/Models/SchemaExtensions/UserModelExtensions.cs
@@ -2,7 +2,9 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Security.Claims;
+using PEngine.Common.Components.Helpers;
 using PEngine.Common.Models.Schema;
+using PEngine.Modules.Member.Models;
 
 namespace PEngine.Common.Models.SchemaExtensions
 {
@@ -13,6 +15,19 @@ namespace PEngine.Common.Models.SchemaExtensions
             var path = $"~/Resources/Profile/{model.Id}";
             
             return File.Exists(path) ? path : null;
+        }
+
+        public static UserModel UpdateUser(this UserModel model, RegisterRequestModel source)
+        {
+            model.UserName = source.UserName;
+            model.Signature = source.Signature;
+
+            if (source.Password != null)
+            {
+                model.PasswordHash = CryptoHelper.Sha256(source.Password);
+            }
+
+            return model;
         }
     }
 }
