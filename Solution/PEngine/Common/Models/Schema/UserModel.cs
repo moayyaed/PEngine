7cf40cb669ad503e83c5cbee4e426d2d7f55a503
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-12-30 00:12:53 +0900

    Tried, but cannot hide phone number

--- Solution/PEngine/Common/Models/Schema/UserModel.cs
+++ Solution/PEngine/Common/Models/Schema/UserModel.cs
@@ -9,8 +9,5 @@ namespace PEngine.Common.Models.Schema
         public string Signature { get; set; }
         public bool IsAdmin { get; set; }
         public DateTime LastLogin { get; set; }
-        
-        private new string PhoneNumber { get; set; }
-        private new string PhoneNumberConfirmed { get; set; }
     }
 }
