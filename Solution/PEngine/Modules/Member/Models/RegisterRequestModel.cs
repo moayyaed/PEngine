2dc496ab331ff9fc946503660747ab94fc22255e
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2019-12-31 10:46:19 +0900

    No email was here?

--- Solution/PEngine/Modules/Member/Models/RegisterRequestModel.cs
+++ Solution/PEngine/Modules/Member/Models/RegisterRequestModel.cs
@@ -2,6 +2,7 @@ namespace PEngine.Modules.Member.Models
 {
     public class RegisterRequestModel
     {
+        public string Email { get; set; }
         public string UserName { get; set; }
         public string Signature { get; set; }
         public string Password { get; set; }
