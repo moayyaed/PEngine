17088018d3dfd09a394375a3233b44389d067ed7
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-01-01 20:01:58 +0900

    Add Some Properties Not to Use IdentityDbContext

--- Solution/PEngine/Common/Components/Database/Contexts/BlogDbContext.cs
+++ Solution/PEngine/Common/Components/Database/Contexts/BlogDbContext.cs
@@ -1,4 +1,8 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PEngine.Common.Models;
 using PEngine.Common.Models.Schema;
 
 namespace PEngine.Common.Components.Database.Contexts
@@ -7,13 +11,25 @@ namespace PEngine.Common.Components.Database.Contexts
     {
         public DbSet<PostModel> Posts { get; set; }
         public DbSet<CommentModel> Comments { get; set; }
+
         public DbSet<UserModel> Users { get; set; }
+        
+        /* To Ditch IdentityDbContext */
+        public DbSet<IdentityRole<long>> Roles { get; set; }
+        public DbSet<IdentityUserClaim<long>> UserClaims { get; set; } 
+        public DbSet<IdentityUserRole<long>> UserRoles { get; set; }
         public DbSet<FileModel> Files { get; set; }
 
         protected BlogDbContext()	
         {	
             Database.EnsureCreated();	
-        }	
+        }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityUserRole<long>>(eb => { eb.HasNoKey(); });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
